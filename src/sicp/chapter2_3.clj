06ea19d16e962e09d7251a12766078ebfefcc394
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2016-11-16 19:22:27 +0900

    Exercise 2.56

--- src/sicp/chapter2_3.clj
+++ src/sicp/chapter2_3.clj
@@ -43,8 +43,96 @@
 
 ;;; 2.3.2  Example: Symbolic Differentiation
 
+(defn variable? [x]
+  (symbol? x))
+
+(defn same-variable? [v1 v2]
+  (and (variable? v1)
+       (variable? v2)
+       (= v1 v2)))
+
+(defn =number? [exp n]
+  (and (number? exp)
+       (= exp n)))
+
+(defn make-sum [a1 a2]
+  (cond
+    (=number? a1 0) a2
+    (=number? a2 0) a1
+    (and (number? a1) (number? a2)) (+ a1 a2)
+    :else (list '+ a1 a2)))
+
+(defn make-product [m1 m2]
+  (cond
+    (or (=number? m1 0) (=number? m2 0)) 0
+    (=number? m1 1) m2
+    (=number? m2 1) m1
+    (and (number? m1) (number? m2)) (* m1 m2)
+    :else (list '* m1 m2)))
+
+(defn sum? [x]
+  (and (list? x)
+       (= (first x) '+)))
+
+(defn addend [s]
+  (fnext s))
+
+(defn augend [s]
+  (first (nnext s)))
+
+(defn product? [x]
+  (and (list? x)
+       (= (first x) '*)))
+
+(defn multiplier [p]
+  (fnext p))
+
+(defn multiplicand [p]
+  (first (nnext p)))
+
+(defn deriv [exp var]
+  (cond
+    (number? exp) 0
+    (variable? exp) (if (same-variable? exp var) 1 0)
+    (sum? exp) (make-sum (deriv (addend exp) var)
+                         (deriv (augend exp) var))
+    (product? exp) (make-sum (make-product (multiplier exp)
+                                           (deriv (multiplicand exp) var))
+                             (make-product (deriv (multiplier exp) var)
+                                           (multiplicand exp)))
+    :else (throw (Exception. (str "unknown expression type -- DERIV " exp)))))
+
 ;; Exercise 2.56
-;; TODO
+(defn make-exponentiation [b e]
+  (cond
+    (=number? e 0) 1
+    (=number? e 1) b
+    :else (list '** b e)))
+
+(defn exponentiation? [x]
+  (and (list? x)
+       (= (first x) '**)))
+
+(defn base [e]
+  (fnext e))
+
+(defn exponent [e]
+  (first (nnext e)))
+
+(defn deriv' [exp var]
+  (cond
+    (number? exp) 0
+    (variable? exp) (if (same-variable? exp var) 1 0)
+    (sum? exp) (make-sum (deriv (addend exp) var)
+                         (deriv (augend exp) var))
+    (product? exp) (make-sum (make-product (multiplier exp)
+                                           (deriv (multiplicand exp) var))
+                             (make-product (deriv (multiplier exp) var)
+                                           (multiplicand exp)))
+    (exponentiation? exp) (make-product (exponent exp)
+                                        (make-exponentiation (base exp)
+                                                             (dec (exponent exp))))
+    :else (throw (Exception. (str "unknown expression type -- DERIV " exp)))))
 
 ;; Exercise 2.57
 ;; TODO
--- test/sicp/chapter2_3_test.clj
+++ test/sicp/chapter2_3_test.clj
@@ -17,6 +17,21 @@
 
 ;;; 2.3.2  Example: Symbolic Differentiation
 
+(deftest deriv-test
+  (is (= (deriv '(+ x 3) 'x) 1))
+  (is (= (deriv '(* x y) 'x) 'y))
+  (is (= (deriv '(* (* x y) (+ x 3)) 'x) '(+ (* x y) (* y (+ x 3))))))
+
+;; Exercise 2.56
+(deftest deriv'-test
+  (is (= (deriv' '(** x 1) 'x) 1))
+  (is (= (deriv' '(** x 2) 'x) '(* 2 x)))
+  (is (= (deriv' '(** x 3) 'x) '(* 3 (** x 2)))))
+
+;; Exercise 2.57
+
+;; Exercise 2.58
+
 ;;; 2.3.3  Example: Representing Sets
 
 ;;; 2.3.4  Example: Huffman Encoding Trees
