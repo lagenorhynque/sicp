4ae9dcd2734b145794e959c21de66504f04c188d
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2016-11-29 15:49:17 +0900

    remove unnecessary scm files, Exercise 2.59

--- src/sicp/chapter2_3.clj
+++ src/sicp/chapter2_3.clj
@@ -12,7 +12,7 @@
 
 ;; Exercise 2.53
 (list 'a 'b 'c)
-;=> (a b c
+;=> (a b c)
 (list (list 'george))
 ;=> ((george))
 (next '((x1 x2) (y1 y2)))
@@ -141,5 +141,35 @@
 ;; TODO
 
 ;;; 2.3.3  Example: Representing Sets
+
+(defn element-of-set? [x set]
+  (cond
+    (empty? set) false
+    (= x (first set)) true
+    :else (recur x (rest set))))
+
+(defn adjoin-set [x set]
+  (if (element-of-set? x set)
+    set
+    (cons x set)))
+
+(defn intersection-set [set1 set2]
+  (cond
+    (or (empty? set1) (empty? set2)) []
+    (element-of-set? (first set1) set2) (cons (first set1)
+                                              (intersection-set (rest set1) set2))
+    :else (intersection-set (rest set1) set2)))
+
+;; Exercise 2.59
+(defn union-set [set1 set2]
+  (cond
+    (empty? set1) set2
+    (empty? set2) set1
+    (element-of-set? (first set1) set2) (union-set (rest set1) set2)
+    :else (cons (first set1)
+                (union-set (rest set1) set2))))
+
+;; Exercise 2.60
+;; TODO
 
 ;;; 2.3.4  Example: Huffman Encoding Trees
--- test/sicp/chapter2_3_test.clj
+++ test/sicp/chapter2_3_test.clj
@@ -34,4 +34,28 @@
 
 ;;; 2.3.3  Example: Representing Sets
 
+(deftest element-of-set?-test
+  (is (false? (element-of-set? 1 [])))
+  (is (true? (element-of-set? 1 [1 2 3])))
+  (is (true? (element-of-set? 3 [1 2 3])))
+  (is (false? (element-of-set? 2 [1 3]))))
+
+(deftest adjoin-set-test
+  (is (= #{1} (set (adjoin-set 1 []))))
+  (is (= #{1 2} (set (adjoin-set 2 [1])))))
+
+(deftest intersection-set-test
+  (is (= #{} (set (intersection-set [] []))))
+  (is (= #{} (set (intersection-set [1] []))))
+  (is (= #{} (set (intersection-set [] [1]))))
+  (is (= #{2} (set (intersection-set [1 2 3] [2]))))
+  (is (= #{2} (set (intersection-set [2] [1 2 3])))))
+
+(deftest union-set-test
+  (is (= #{} (set (union-set [] []))))
+  (is (= #{1} (set (union-set [1] []))))
+  (is (= #{1} (set (union-set [] [1]))))
+  (is (= #{1 2 3} (set (union-set [1 3] [2]))))
+  (is (= #{1 2 3} (set (union-set [2] [1 3])))))
+
 ;;; 2.3.4  Example: Huffman Encoding Trees
