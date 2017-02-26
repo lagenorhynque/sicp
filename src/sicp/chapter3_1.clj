6feb1ad6c04c69e9ac7b589a2f74855d35d40bb2
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-02-27 01:28:27 +0900

    Exercise 3.3, 3.4

--- src/sicp/chapter3_1.clj
+++ src/sicp/chapter3_1.clj
@@ -37,8 +37,9 @@
                    (case m
                      :withdraw withdraw
                      :deposit deposit
-                     (throw (Exception. (str "Unknown request -- make-account "
-                                             m)))))]
+                     (throw (IllegalArgumentException.
+                             (str "Unknown request -- make-account "
+                                  m)))))]
     dispatch))
 
 ;; Exercise 3.1
@@ -46,7 +47,6 @@
   (let [sum (atom init)]
     (fn [n]
       (swap! sum + n))))
-
 
 ;; Exercise 3.2
 (defn make-monitored [f]
@@ -57,3 +57,67 @@
         :reset-count (reset! cnt 0)
         (do (swap! cnt inc)
             (f x))))))
+
+;; Exercise 3.3
+(defn make-account' [balance password]
+  (let [balance (atom balance)
+        withdraw (fn [amount]
+                   (if (>= @balance amount)
+                     (swap! balance - amount)
+                     "Insufficient funds"))
+        deposit (fn [amount]
+                  (swap! balance + amount))
+        exec-if-correct (fn [pass f]
+                          (fn [amount]
+                            (if (= pass password)
+                              (f amount)
+                              "Incorrect password")))
+        dispatch (fn [pass m]
+                   (case m
+                     :withdraw (exec-if-correct pass withdraw)
+                     :deposit (exec-if-correct pass deposit)
+                     (throw (IllegalArgumentException.
+                             (str "Unknown request -- make-account "
+                                  m)))))]
+    dispatch))
+
+;; Exercise 3.4
+(defn call-the-cops []
+  "Calling the cops...")
+
+(defn make-account'' [balance password]
+  (let [balance (atom balance)
+        withdraw (fn [amount]
+                   (if (>= @balance amount)
+                     (swap! balance - amount)
+                     "Insufficient funds"))
+        deposit (fn [amount]
+                  (swap! balance + amount))
+        incorrect-cnt (atom 0)
+        exec-if-correct (fn [pass f]
+                          (fn [amount]
+                            (if (= pass password)
+                              (do (reset! incorrect-cnt 0)
+                                  (f amount))
+                              (do (swap! incorrect-cnt inc)
+                                  (if (> @incorrect-cnt 7)
+                                    (call-the-cops)
+                                    "Incorrect password")))))
+        dispatch (fn [pass m]
+                   (case m
+                     :withdraw (exec-if-correct pass withdraw)
+                     :deposit (exec-if-correct pass deposit)
+                     (throw (IllegalArgumentException.
+                             (str "Unknown request -- make-account "
+                                  m)))))]
+    dispatch))
+
+;;; 3.1.2  The Benefits of Introducing Assignment
+
+;; Exercise 3.5
+
+
+;; Exercise 3.6
+
+
+;;; 3.1.3  The Costs of Introducing Assignment
