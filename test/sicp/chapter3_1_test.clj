960aa796e6fb4d0deb6ee4f154ae76336f0e620b
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-03-01 18:14:02 +0900

    fix tests for Exercise 3.1-3.6

--- test/sicp/chapter3_1_test.clj
+++ test/sicp/chapter3_1_test.clj
@@ -9,25 +9,34 @@
 ;; Exercise 3.1
 (deftest make-accumulator-test
   (let [acc (make-accumulator 5)]
-    (is 15 (acc 10))
-    (is 25 (acc 10))))
+    (is (= 15 (acc 10)))
+    (is (= 25 (acc 10)))))
 
 ;; Exercise 3.2
 (deftest make-monitored-test
   (let [s (make-monitored #(Math/sqrt %))]
-    (is 10.0 (s 100))
-    (is 1 (s :how-many-calls?))))
+    (is (= 10.0 (s 100)))
+    (is (= 1 (s :how-many-calls?)))))
 
 ;; Exercise 3.3
 (deftest make-account'-test
   (let [acc (make-account' 100 :secret-password)]
-    (is 60 ((acc :secret-password :withdraw) 40))
-    (is "Incorrect password" ((acc :some-other-password :deposit) 50))))
+    (is (= 60 ((acc :secret-password :withdraw) 40)))
+    (is (= "Incorrect password" ((acc :some-other-password :deposit) 50)))))
 
 ;; Exercise 3.4
 (deftest make-account''-test
   (let [acc (make-account'' 100 :secret-password)]
-    (is "Incorrect password" (do (dotimes [_ 6]
-                                    ((acc :some-other-password :withdraw) 40))
-                                 ((acc :some-other-password :withdraw) 40)))
-    (is "Calling the cops" ((acc :some-other-password :withdraw) 40))))
+    (is (every? #(= % "Incorrect password")
+                (repeatedly 7 #((acc :some-other-password :withdraw) 40))))
+    (is (= "Calling the cops..." ((acc :some-other-password :withdraw) 40)))))
+
+;; Exercise 3.5
+(deftest estimate-pi'-test
+  (is (< 3 (estimate-pi' 10000) 4)))
+
+;; Exercise 3.6
+(deftest rand'-test
+  (let [init @random-init]
+    (is (= (rand-update init) (rand' :generate)))
+    (is (= 10 ((rand' :reset) 10)))))
