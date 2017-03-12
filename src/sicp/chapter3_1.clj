2b63ee8f3cf07d626bbbde411a35b15997f0798a
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-03-13 02:00:21 +0900

    Exercise 3.7, 3,8

--- src/sicp/chapter3_1.clj
+++ src/sicp/chapter3_1.clj
@@ -163,3 +163,40 @@
                      m)))))))
 
 ;;; 3.1.3  The Costs of Introducing Assignment
+
+;; Exercise 3.7
+(defn make-account''' [balance password]
+  (let [balance (atom balance)
+        withdraw (fn [amount]
+                   (if (>= @balance amount)
+                     (swap! balance - amount)
+                     "Insufficient funds"))
+        deposit (fn [amount]
+                  (swap! balance + amount))
+        correct? #(= % password)
+        dispatch (fn [pass m]
+                   (if (= m :correct?)
+                     (correct? pass)
+                     (if (correct? pass)
+                       (case m
+                         :withdraw withdraw
+                         :deposit deposit
+                         (throw (IllegalArgumentException.
+                                 (str "Unknown request -- make-account "
+                                      m))))
+                       (fn [_] "Incorrect password"))))]
+    dispatch))
+
+(defn make-joint [account password new-password]
+  (if (account password :correct?)
+    (fn [pass m]
+      (if (= pass new-password)
+        (account password m)
+        (fn [_] "Incorrect password")))
+    (throw (IllegalArgumentException. "Incorrect password"))))
+
+;; Exercise 3.8
+(def f
+  (let [n (atom 2)]
+    (fn [x]
+      (swap! n #(- 1 % x)))))
--- test/sicp/chapter3_1_test.clj
+++ test/sicp/chapter3_1_test.clj
@@ -37,5 +37,21 @@
 
 ;; Exercise 3.6
 (deftest rand'-test
-  (is (= (rand-update random-init) (rand' :generate)))
-  (is (= 10 ((rand' :reset) 10))))
+  (is (= 10 ((rand' :reset) 10)))
+  (is (= 11 (rand' :generate))))
+
+;; Exercise 3.7
+(deftest make-joint-test
+  (let [peter-acc (make-account''' 100 :open-sesame)]
+    (is (thrown-with-msg? IllegalArgumentException
+                          #"Incorrect password"
+                          (make-joint peter-acc :open-bean :rosebud)))
+    (let [paul-acc (make-joint peter-acc :open-sesame :rosebud)]
+      (is (= 70 ((paul-acc :rosebud :withdraw) 30)))
+      (is (= 120 ((paul-acc :rosebud :deposit) 50)))
+      (is (= "Incorrect password" ((paul-acc :roseblue :withdraw) 20))))))
+
+;; Exercise 3.8
+(deftest f-test
+  (is (= 0 (+ (f 0) (f 1))))
+  (is (= 1 (+ (f 1) (f 0)))))
