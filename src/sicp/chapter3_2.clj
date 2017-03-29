47981d4eb504caf4ad78c2b2804aad717542040f
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-03-29 23:36:23 +0900

    Exercise 3.10

--- src/sicp/chapter3_2.clj
+++ src/sicp/chapter3_2.clj
@@ -145,7 +145,71 @@
 ;;; 3.2.3  Frames as the Repository of Local State
 
 ;; Exercise 3.10
-;; TODO
+(def make-withdraw-define-w1
+  (-> [(d/node-attrs {:shape :record})
+       [:g (create-node "global env"
+                        #sicp/s "make-withdraw
+                                |W1")]
+       [:e1 (create-node "E1"
+                         "initial-amount: 100")]
+       [:e2 (create-node "E2"
+                         "balance: 100")]
+
+       [:e1 :g]
+       [:e2 :e1]]
+      d/digraph
+      d/dot))
+
+(def make-withdraw-w1
+  (-> [(d/node-attrs {:shape :record})
+       [:g (create-node "global env"
+                        #sicp/s "make-withdraw
+                                |acc")]
+       [:e1 (create-node "E1"
+                         "initial-amount: 100")]
+       [:e2 (create-node "E2"
+                         "balance: 100")]
+       [:e3 (create-node "E3"
+                         "amount: 50")]
+
+       (d/node-attrs {:shape :note})
+       (d/subgraph
+        [[:node (create-node
+                 #sicp/s "(if (>= balance amount)
+                         |    (begin (set! balance (- balance amount))
+                         |           balance)
+                         |    \"Insufficient funds\")")]
+         :c3])
+
+       [:e1 :g]
+       [:e2 :e1]
+       [:e3 :e2]
+
+       (d/edge-attrs {:dir :none})
+       [:c3 :e3]]
+      d/digraph
+      d/dot))
+
+(def make-withdraw-define-w2
+  (-> [(d/node-attrs {:shape :record})
+       [:g (create-node "global env"
+                        #sicp/s "make-withdraw
+                                |W1")]
+       [:e1 (create-node "E1"
+                         "initial-amount: 100")]
+       [:e2 (create-node "E2"
+                         "balance: 50")]
+       [:e4 (create-node "E4"
+                         "initial-amount: 100")]
+       [:e5 (create-node "E5"
+                         "balance: 100")]
+
+       [:e1 :g]
+       [:e2 :e1]
+       [:e4 :g]
+       [:e5 :e4]]
+      d/digraph
+      d/dot))
 
 ;;; 3.2.4  Internal Definitions
 
