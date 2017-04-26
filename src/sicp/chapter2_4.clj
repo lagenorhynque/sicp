59a293ab065fb30257863c33bef7306d0e58ff4d
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-04-27 00:17:05 +0900

    add Chapter 2.4, 2.5 files

---
+++ src/sicp/chapter2_4.clj
@@ -0,0 +1,9 @@
+(ns sicp.chapter2-4)
+
+;;;; 2.4  Multiple Representations for Abstract Data
+
+;;; 2.4.1  Representations for Complex Numbers
+
+;;; 2.4.2  Tagged data
+
+;;; 2.4.3  Data-Directed Programming and Additivity
