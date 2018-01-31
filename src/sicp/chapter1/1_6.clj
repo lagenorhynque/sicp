fb31798c5d92ed03b6b1e7d8e4eeeb9b026b2c1b
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-02-01 02:33:55 +0900

    1.1.6  Conditional Expressions and Predicates

--- src/sicp/chapter1/1_6.clj
+++ src/sicp/chapter1/1_6.clj
@@ -4,9 +4,46 @@
 
 ;;; 1.1.6  Conditional Expressions and Predicates
 
+(defn abs [x]
+  (cond (> x 0) x
+        (= x 0) 0
+        (< x 0) (- x)))
+
+(defn abs' [x]
+  (cond (< x 0) (- x)
+        :else x))
+
+(defn abs'' [x]
+  (if (< x 0)
+    (- x)
+    x))
+
+;; (and (> x 5) (< x 10))
+;; (< 5 x 10)
+
+(defn >=' [x y]
+  (or (> x y) (= x y)))
+
+(defn >='' [x y]
+  (not (< x y)))
+
 ;; Exercise 1.1
 10
 ;=> 10
 
 (+ 5 3 4)
 ;=> 12
+
+;; TODO
+
+;; Exercise 1.2
+;; TODO
+
+;; Exercise 1.3
+;; TODO
+
+;; Exercise 1.4
+;; TODO
+
+;; Exercise 1.5
+;; TODO
