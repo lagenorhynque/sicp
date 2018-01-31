d0a2ea40d4429999a08eacd9abe5ad6f8b006d36
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-02-01 00:51:31 +0900

    1.1.4  Compound Procedures

--- src/sicp/chapter1/1_4.clj
+++ src/sicp/chapter1/1_4.clj
@@ -3,3 +3,21 @@
 ;;;; 1.1  The Elements of Programming
 
 ;;; 1.1.4  Compound Procedures
+
+(defn square [x] (* x x))
+
+(square 21)
+
+(square (+ 2 5))
+
+(square (square 3))
+
+(defn sum-of-squares [x y]
+  (+ (square x) (square y)))
+
+(sum-of-squares 3 4)
+
+(defn f [a]
+  (sum-of-squares (+ a 1) (* a 2)))
+
+(f 5)
