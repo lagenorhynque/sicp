2bfbdeeddff8d726206dd214f2211f482c3dc4d6
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-02-01 00:24:41 +0900

    1.1.2  Naming and the Environment

--- src/sicp/chapter1/1_2.clj
+++ src/sicp/chapter1/1_2.clj
@@ -3,3 +3,19 @@
 ;;;; 1.1  The Elements of Programming
 
 ;;; 1.1.2  Naming and the Environment
+
+(def size 2)
+
+size
+
+(* 5 size)
+
+(def pi 3.14159)
+
+(def radius 10)
+
+(* pi (* radius radius))
+
+(def circumference (* 2 pi radius))
+
+circumference
