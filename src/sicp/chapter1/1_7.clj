68063b5a74619f10ecaefdad349d30259939baae
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-02-10 00:47:50 +0900

    Exercise 1.7

--- src/sicp/chapter1/1_7.clj
+++ src/sicp/chapter1/1_7.clj
@@ -50,7 +50,7 @@
 ;; > (square-iter' 1.0 2)
 ;; java.lang.StackOverflowError
 ;;
-;; Reason: Since all the argments of `new-if` are always evaluated even if `predicate` is evaluated to false,
+;; Reason: Since all the arguments of `new-if` are always evaluated even if `predicate` is evaluated to false,
 ;;   recursion does not stop forever. You can control evaluation of arguments using macros as follows:
 
 (defmacro new-if' [predicate then-clause else-clause]
@@ -66,7 +66,26 @@
 (square-iter'' 1.0 2)
 
 ;; Exercise 1.7
-;; TODO
+;; When applied to very small numbers,
+;; e.g. (sqrt 1.0e-20)
+;; returns inaccurate results.
+;;
+;; When applied to very large numbers,
+;; e.g. (sqrt 1.0e50)
+;; takes too much time to calculate results.
+
+(defn good-enough?' [old-guess new-guess]
+  (< (abs (- old-guess new-guess)) 0.001))
+
+(defn square-iter'' [old-guess new-guess x]
+  (if (good-enough?' old-guess new-guess)
+    new-guess
+    (recur new-guess
+           (improve new-guess x)
+           x)))
+
+(defn sqrt' [x]
+  (square-iter'' 0 1.0 x))
 
 ;; Exercise 1.8
 ;; TODO
