2b02a9ecfaa994bd95d7fc9da963d57aa1c4f568
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-03-06 20:34:39 +0900

    1.2.4  Exponentiation (Common Lisp)

--- src/sicp/chapter1/2-4.lisp
+++ src/sicp/chapter1/2-4.lisp
@@ -1,7 +1,43 @@
 (defpackage sicp.chapter1.2-4
-  (:use :cl))
+  (:use :cl)
+  (:import-from :sicp.chapter1.1-4 square))
 (in-package :sicp.chapter1.2-4)
 
 ;;;; 1.2  Procedures and the Processes They Generate
 
 ;;; 1.2.4  Exponentiation
+
+(defun expt* (b n)
+  (if (zerop n)
+      1
+      (* b (expt* b (1- n)))))
+
+(defun expt-iter (b counter product)
+  (if (zerop counter)
+      product
+      (expt-iter b
+                 (1- counter)
+                 (* b product))))
+
+(defun expt** (b n)
+  (expt-iter b n 1))
+
+(defun evenp* (n)
+  (zerop (rem n 2)))
+
+(defun fast-expt (b n)
+  (cond ((zerop n) 1)
+        ((evenp* n) (square (fast-expt b (/ n 2))))
+        (t (* b (fast-expt b (1- n))))))
+
+;; Exercise 1.16
+;; TODO
+
+;; Exercise 1.17
+;; TODO
+
+;; Exercise 1.18
+;; TODO
+
+;; Exercise 1.19
+;; TODO
