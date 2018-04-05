15e20a59f1ba91d4455f7ca8eff9d290602f0e37
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-04-05 16:23:58 +0900

    1.2.5  Greatest Common Divisors

--- src/sicp/chapter1/2-5.lisp
+++ src/sicp/chapter1/2-5.lisp
@@ -5,3 +5,11 @@
 ;;;; 1.2  Procedures and the Processes They Generate
 
 ;;; 1.2.5  Greatest Common Divisors
+
+(defun gcd* (a b)
+  (if (zerop b)
+      a
+      (gcd* b (rem a b))))
+
+;; Exercise 1.20
+;; TODO
--- src/sicp/chapter1/2_5.clj
+++ src/sicp/chapter1/2_5.clj
@@ -3,3 +3,11 @@
 ;;;; 1.2  Procedures and the Processes They Generate
 
 ;;; 1.2.5  Greatest Common Divisors
+
+(defn gcd [a b]
+  (if (zero? b)
+    a
+    (recur b (rem a b))))
+
+;; Exercise 1.20
+;; TODO
