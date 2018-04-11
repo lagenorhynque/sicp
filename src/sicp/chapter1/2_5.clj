d4f253970aac888a85de2ac5df13c28af48aeff7
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-04-11 14:37:32 +0900

    Exercise 1.20

--- src/sicp/chapter1/2_5.clj
+++ src/sicp/chapter1/2_5.clj
@@ -10,4 +10,47 @@
     (recur b (rem a b))))
 
 ;; Exercise 1.20
-;; TODO
+;; normal-order evaluation
+(gcd 206 40)
+(gcd 40 (rem 206 40))
+;; (if (zero? (rem 206 40)) => 1
+(gcd (rem 206 40)
+     (rem 40
+          (rem 206 40)))
+;; (if (zero? (rem 40
+;;                 (rem 206 40))) => 2
+(gcd (rem 40
+          (rem 206 40))
+     (rem (rem 206 40)
+          (rem 40
+               (rem 206 40))))
+;; (if (zero? (rem (rem 206 40)
+;;                 (rem 40
+;;                      (rem 206 40)))) => 4
+(gcd (rem (rem 206 40)
+          (rem 40
+               (rem 206 40)))
+     (rem (rem 40
+               (rem 206 40))
+          (rem (rem 206 40)
+               (rem 40 (rem 206 40)))))
+;; (if (zero? (rem (rem 40
+;;                      (rem 206 40))
+;;                 (rem (rem 206 40)
+;;                      (rem 40 (rem 206 40))))) => 7
+;;   (rem (rem 206 40)
+;;        (rem 40
+;;             (rem 206 40))) => 4
+;; 18 `rem` operations are performed.
+
+;; applicative-order evaluation
+(gcd 206 40)
+;; (gcd 40 (rem 206 40)) => 1
+(gcd 40 6)
+;; (gcd 6 (rem 40 6)) => 1
+(gcd 6 4)
+;; (gcd 4 (rem 6 4)) => 1
+(gcd 4 2)
+;; (gcd 2 (rem 4 2)) => 1
+(gcd 2 0)
+;; 4 `rem` operations are performed.
