536c3e1f198510fbdd692d564f7dfd110c814c9f
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-06-05 22:10:59 +0900

    1.3.4  Procedures as Returned Values

--- src/sicp/chapter1/3-4.lisp
+++ src/sicp/chapter1/3-4.lisp
@@ -1,7 +1,78 @@
 (defpackage sicp.chapter1.3-4
-  (:use :cl))
+  (:use :cl)
+  (:import-from :sicp.chapter1.1-4 square)
+  (:import-from :sicp.chapter1.1-7 average)
+  (:import-from :sicp.chapter1.3-3 fixed-point))
 (in-package :sicp.chapter1.3-4)
 
 ;;;; 1.3  Formulating Abstractions with Higher-Order Procedures
 
 ;;; 1.3.4  Procedures as Returned Values
+
+(defun average-damp (f)
+  (lambda (x) (average x (funcall f x))))
+
+(funcall (average-damp #'square) 10)
+
+(defun sqrt* (x)
+  (fixed-point (average-damp (lambda (y) (/ x y)))
+               1.0))
+
+(defun cube-root (x)
+  (fixed-point (average-damp (lambda (y) (/ x (square y))))
+               1.0))
+
+(defparameter dx 0.00001)
+
+(defun deriv (g)
+  (lambda (x)
+    (/ (- (funcall g (+ x dx)) (funcall g x))
+       dx)))
+
+(defun cube (x) (* x x x))
+(funcall (deriv #'cube) 5)
+
+(defun newton-transform (g)
+  (lambda (x)
+    (- x (/ (funcall g x) (funcall (deriv g) x)))))
+
+(defun newtons-method (g guess)
+  (fixed-point (newton-transform g) guess))
+
+(defun sqrt** (x)
+  (newtons-method (lambda (y) (- (square y) x))
+                  1.0))
+
+(defun fixed-point-of-transform (g transform guess)
+  (fixed-point (funcall transform g) guess))
+
+(defun sqrt*** (x)
+  (fixed-point-of-transform (lambda (y) (/ x y))
+                            #'average-damp
+                            1.0))
+
+(defun sqrt**** (x)
+  (fixed-point-of-transform (lambda (y) (- (square y) x))
+                            #'newton-transform
+                            1.0))
+
+;; Exercise 1.40
+;; TODO
+
+;; Exercise 1.41
+;; TODO
+
+;; Exercise 1.42
+;; TODO
+
+;; Exercise 1.43
+;; TODO
+
+;; Exercise 1.44
+;; TODO
+
+;; Exercise 1.45
+;; TODO
+
+;; Exercise 1.46
+;; TODO
--- src/sicp/chapter1/3_4.clj
+++ src/sicp/chapter1/3_4.clj
@@ -1,5 +1,76 @@
-(ns sicp.chapter1.3-4)
+(ns sicp.chapter1.3-4
+  (:require [sicp.chapter1.1-4 :refer [square]]
+            [sicp.chapter1.1-7 :refer [average]]
+            [sicp.chapter1.3-3 :refer [fixed-point]]))
 
 ;;;; 1.3  Formulating Abstractions with Higher-Order Procedures
 
 ;;; 1.3.4  Procedures as Returned Values
+
+(defn average-damp [f]
+  (fn [x] (average x (f x))))
+
+((average-damp square) 10)
+
+(defn sqrt [x]
+  (fixed-point (average-damp (fn [y] (/ x y)))
+               1.0))
+
+(defn cube-root [x]
+  (fixed-point (average-damp (fn [y] (/ x (square y))))
+               1.0))
+
+(def dx 0.00001)
+
+(defn deriv [g]
+  (fn [x]
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(defn cube [x] (* x x x))
+((deriv cube) 5)
+
+(defn newton-transform [g]
+  (fn [x]
+    (- x (/ (g x) ((deriv g) x)))))
+
+(defn newtons-method [g guess]
+  (fixed-point (newton-transform g) guess))
+
+(defn sqrt' [x]
+  (newtons-method (fn [y] (- (square y) x))
+                  1.0))
+
+(defn fixed-point-of-transform [g transform guess]
+  (fixed-point (transform g) guess))
+
+(defn sqrt'' [x]
+  (fixed-point-of-transform (fn [y] (/ x y))
+                            average-damp
+                            1.0))
+
+(defn sqrt''' [x]
+  (fixed-point-of-transform (fn [y] (- (square y) x))
+                            newton-transform
+                            1.0))
+
+;; Exercise 1.40
+;; TODO
+
+;; Exercise 1.41
+;; TODO
+
+;; Exercise 1.42
+;; TODO
+
+;; Exercise 1.43
+;; TODO
+
+;; Exercise 1.44
+;; TODO
+
+;; Exercise 1.45
+;; TODO
+
+;; Exercise 1.46
+;; TODO
