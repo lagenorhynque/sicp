1dfde1a0ba145323ed6b38ececd91347b45e3e21
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-06-05 15:23:55 +0900

    1.3.3  Procedures as General Methods

--- src/sicp/chapter1/3-3.lisp
+++ src/sicp/chapter1/3-3.lisp
@@ -1,7 +1,69 @@
 (defpackage sicp.chapter1.3-3
-  (:use :cl))
+  (:use :cl)
+  (:import-from :sicp.chapter1.1-7 average))
 (in-package :sicp.chapter1.3-3)
 
 ;;;; 1.3  Formulating Abstractions with Higher-Order Procedures
 
 ;;; 1.3.3  Procedures as General Methods
+
+(defun close-enoughp (x y)
+  (< (abs (- x y)) 0.001))
+
+(defun search-root (f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enoughp neg-point pos-point)
+        midpoint
+        (let ((test-value (funcall f midpoint)))
+          (cond ((plusp test-value) (search-root f neg-point midpoint))
+                ((minusp test-value) (search-root f midpoint pos-point))
+                (t midpoint))))))
+
+(defun half-interval-method (f a b)
+  (let ((a-value (funcall f a))
+        (b-value (funcall f b)))
+    (cond ((and (minusp a-value) (plusp b-value)) (search-root f a b))
+          ((and (minusp b-value) (plusp a-value)) (search-root f b a))
+          (t (error (format nil "Values are not of oppsite sign: ~a ~a" a b))))))
+
+(half-interval-method #'sin 2.0 4.0)
+
+(half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3))
+                      1.0
+                      2.0)
+
+(defparameter tolerance 0.00001)
+
+(defun fixed-point (f first-guess)
+  (labels ((close-enoughp (v1 v2)
+             (< (abs (- v1 v2)) tolerance))
+           (try (guess)
+             (let ((nxt (funcall f guess)))
+               (if (close-enoughp guess nxt)
+                   nxt
+                   (try nxt)))))
+    (try first-guess)))
+
+(fixed-point #'cos 1.0)
+
+(fixed-point (lambda (y) (+ (sin y) (cos y)))
+             1.0)
+
+(defun sqrt* (x)
+  (fixed-point (lambda (y) (average y (/ x y)))
+               1.0))
+
+;; Exercise 1.35
+;; TODO
+
+;; Exercise 1.36
+;; TODO
+
+;; Exercise 1.37
+;; TODO
+
+;; Exercise 1.38
+;; TODO
+
+;; Exercise 1.39
+;; TODO
--- src/sicp/chapter1/3_3.clj
+++ src/sicp/chapter1/3_3.clj
@@ -1,5 +1,71 @@
-(ns sicp.chapter1.3-3)
+(ns sicp.chapter1.3-3
+  (:require [clojure.math.numeric-tower :refer [abs]]
+            [sicp.chapter1.1-7 :refer [average]]))
 
 ;;;; 1.3  Formulating Abstractions with Higher-Order Procedures
 
 ;;; 1.3.3  Procedures as General Methods
+
+(defn close-enough? [x y]
+  (< (abs (- x y)) 0.001))
+
+(defn search [f neg-point pos-point]
+  (let [midpoint (average neg-point pos-point)]
+    (if (close-enough? neg-point pos-point)
+      midpoint
+      (let [test-value (f midpoint)]
+        (cond
+          (pos? test-value) (recur f neg-point midpoint)
+          (neg? test-value) (recur f midpoint pos-point)
+          :else midpoint)))))
+
+(defn half-interval-method [f a b]
+  (let [a-value (f a)
+        b-value (f b)]
+    (cond
+      (and (neg? a-value) (pos? b-value)) (search f a b)
+      (and (neg? b-value) (pos? a-value)) (search f b a)
+      :else (throw (IllegalArgumentException.
+                    (format "Values are not of oppsite sign: %s %s" a b))))))
+
+(half-interval-method #(Math/sin %) 2.0 4.0)
+
+(half-interval-method (fn [x] (- (* x x x) (* 2 x) 3))
+                      1.0
+                      2.0)
+
+(def tolerance 0.00001)
+
+(defn fixed-point [f first-guess]
+  (letfn [(close-enough? [v1 v2]
+            (< (abs (- v1 v2)) tolerance))
+          (try-it [guess]
+            (let [nxt (f guess)]
+              (if (close-enough? guess nxt)
+                nxt
+                (recur nxt))))]
+    (try-it first-guess)))
+
+(fixed-point #(Math/cos %) 1.0)
+
+(fixed-point (fn [y] (+ (Math/sin y) (Math/cos y)))
+             1.0)
+
+(defn sqrt [x]
+  (fixed-point (fn [y] (average y (/ x y)))
+               1.0))
+
+;; Exercise 1.35
+;; TODO
+
+;; Exercise 1.36
+;; TODO
+
+;; Exercise 1.37
+;; TODO
+
+;; Exercise 1.38
+;; TODO
+
+;; Exercise 1.39
+;; TODO
