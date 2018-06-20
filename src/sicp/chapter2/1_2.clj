1bd58e75aa4255c32bdb827e99cbc6600d0fa88e
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-06-21 02:04:39 +0900

    2.1.2  Abstraction Barriers

--- src/sicp/chapter2/1_2.clj
+++ src/sicp/chapter2/1_2.clj
@@ -1,5 +1,24 @@
-(ns sicp.chapter2.1-2)
+(ns sicp.chapter2.1-2
+  (:require [sicp.chapter1.2-5 :refer [gcd]]
+            [sicp.common.list :refer :all]))
 
 ;;;; 2.1  Introduction to Data Abstraction
 
 ;;; 2.1.2  Abstraction Barriers
+
+(defn make-rat [n d]
+  (kons n d))
+
+(defn numer [x]
+  (let [g (gcd (car x) (cdr x))]
+    (/ (car x) g)))
+
+(defn denom [x]
+  (let [g (gcd (car x) (cdr x))]
+    (/ (cdr x) g)))
+
+;; Exercise 2.2
+;; TODO
+
+;; Exercise 2.3
+;; TODO
