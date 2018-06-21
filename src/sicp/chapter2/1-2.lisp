9248ca2c5e73258f61c0d35975178ed3705a6d54
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-06-21 15:12:05 +0900

    2.1.2  Abstraction Barriers (Common Lisp)

--- src/sicp/chapter2/1-2.lisp
+++ src/sicp/chapter2/1-2.lisp
@@ -5,3 +5,20 @@
 ;;;; 2.1  Introduction to Data Abstraction
 
 ;;; 2.1.2  Abstraction Barriers
+
+(defun make-rat (n d)
+  (cons n d))
+
+(defun numer (x)
+  (let ((g (gcd (car x) (cdr x))))
+    (/ (car x) g)))
+
+(defun denom (x)
+  (let ((g (gcd (car x) (cdr x))))
+    (/ (cdr x) g)))
+
+;; Exercise 2.2
+;; TODO
+
+;; Exercise 2.3
+;; TODO
