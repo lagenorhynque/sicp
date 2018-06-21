0227bc0a849200c810cdc09d15b2309f4ca05adb
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-06-21 20:02:54 +0900

    2.1.1  Example: Arithmetic Operations for Rational Numbers (Scheme)

---
+++ src/sicp/chapter2_1.scm
@@ -0,0 +1,87 @@
+;;;; 2.1  Introduction to Data Abstraction
+
+;;; 2.1.1  Example: Arithmetic Operations for Rational Numbers
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(define x (cons 1 2))
+
+(car x)
+
+(cdr x)
+
+(define y (cons 3 4))
+
+(define z (cons x y))
+
+(car (car z))
+
+(car (cdr z))
+
+(define (make-rat n d) (cons n d))
+
+(define (numer x) (car x))
+
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (display (numer x))
+  (display "/")
+  (display (denom x))
+  (newline))
+
+(define one-half (make-rat 1 2))
+(print-rat one-half)
+
+(define one-third (make-rat 1 3))
+(print-rat (add-rat one-half one-third))
+
+(print-rat (mul-rat one-half one-third))
+
+(print-rat (add-rat one-third one-third))
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g) (/ d g))))
+
+(print-rat (add-rat one-third one-third))
+
+;; Exercise 2.1
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (if (negative? d)
+        (cons (/ (- n) g) (/ (- d) g))
+        (cons (/ n g) (/ d g)))))
+
+(define (make-rat* n d)
+  (let ((g (gcd n d))
+        (f (if (negative? d)
+               -
+               identity)))
+    (cons (/ (f n) g) (/ (f d) g))))
+
+;;; 2.1.2  Abstraction Barriers
+
+;;; 2.1.3  What Is Meant by Data?
+
+;;; 2.1.4  Extended Exercise: Interval Arithmetic
