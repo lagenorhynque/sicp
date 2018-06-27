e2eede746e5a3554e31af5e841ba8687e331112c
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-06-27 21:55:33 +0900

    2.1.3  What Is Meant by Data? (Common Lisp)

--- src/sicp/chapter2/1-3.lisp
+++ src/sicp/chapter2/1-3.lisp
@@ -5,3 +5,24 @@
 ;;;; 2.1  Introduction to Data Abstraction
 
 ;;; 2.1.3  What Is Meant by Data?
+
+(defun kons (x y)
+  (labels ((dispatch (m)
+             (case m
+               (0 x)
+               (1 y)
+               (otherwise (error (format nil "Argument not 0 or 1 -- CONS ~a" m))))))
+    #'dispatch))
+
+(defun kar (z) (funcall z 0))
+
+(defun kdr (z) (funcall z 1))
+
+;; Exercise 2.4
+;; TODO
+
+;; Exercise 2.5
+;; TODO
+
+;; Exercise 2.6
+;; TODO
