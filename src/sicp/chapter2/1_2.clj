d8b11bbc7ec5031159ead66a14118ed16954373a
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-06-28 19:03:01 +0900

    Exercise 2.2

--- src/sicp/chapter2/1_2.clj
+++ src/sicp/chapter2/1_2.clj
@@ -1,5 +1,6 @@
 (ns sicp.chapter2.1-2
-  (:require [sicp.chapter1.2-5 :refer [gcd]]
+  (:require [sicp.chapter1.1-7 :refer [average]]
+            [sicp.chapter1.2-5 :refer [gcd]]
             [sicp.common.list :refer :all]))
 
 ;;;; 2.1  Introduction to Data Abstraction
@@ -18,7 +19,32 @@
     (/ (cdr x) g)))
 
 ;; Exercise 2.2
-;; TODO
+(defn make-point [x y]
+  (kons x y))
+(defn x-point [p]
+  (car p))
+(defn y-point [p]
+  (cdr p))
+
+(defn make-segment [start end]
+  (kons start end))
+(defn start-segment [s]
+  (car s))
+(defn end-segment [s]
+  (cdr s))
+
+(defn midpoint-segment [s]
+  (let [start (start-segment s)
+        start-x (x-point start)
+        start-y (y-point start)
+        end (end-segment s)
+        end-x (x-point end)
+        end-y (y-point end)]
+    (make-point (average start-x end-x)
+                (average start-y end-y))))
+
+(defn print-point [p]
+  (printf "(%s,%s)%n" (x-point p) (y-point p)))
 
 ;; Exercise 2.3
 ;; TODO
