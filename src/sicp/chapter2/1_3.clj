80e39206fbdc0520b88ccd8c9a852d2d349184a6
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-07-12 20:55:42 +0900

    Exercise 2.4, 2.5, 2.6

--- src/sicp/chapter2/1_3.clj
+++ src/sicp/chapter2/1_3.clj
@@ -1,4 +1,5 @@
-(ns sicp.chapter2.1-3)
+(ns sicp.chapter2.1-3
+  (:require [clojure.math.numeric-tower :refer [expt]]))
 
 ;;;; 2.1  Introduction to Data Abstraction
 
@@ -18,10 +19,55 @@
 (defn cdr [z] (z 1))
 
 ;; Exercise 2.4
-;; TODO
+(defn kons' [x y]
+  (fn [m] (m x y)))
+(defn car' [z]
+  (z (fn [p q] p)))
+
+;; (car' (kons' x y))
+;; (car' (fn [m] (m x y)))
+;; ((fn [m] (m x y)) (fn [p q] p))
+;; ((fn [p q] p) x y)
+;; x
+
+(defn cdr' [z]
+  (z (fn [p q] q)))
 
 ;; Exercise 2.5
-;; TODO
+(defn kons'' [x y]
+  (* (expt 2 x)
+     (expt 3 y)))
+
+(defn- count-factor
+  ([x n] (count-factor x n 0))
+  ([x n acc]
+   (if (zero? (mod x n))
+     (recur (quot x n) n (inc acc))
+     acc)))
+
+(defn car'' [z]
+  (count-factor z 2))
+
+(defn cdr'' [z]
+  (count-factor z 3))
 
 ;; Exercise 2.6
-;; TODO
+(def zero (fn [f] (fn [x] x)))
+(defn add-1 [n]
+  (fn [f] (fn [x] (f ((n f) x)))))
+
+;; define `one`
+(add-1 zero)
+(fn [f] (fn [x] (f (((fn [f] (fn [x] x)) f) x))))
+(fn [f] (fn [x] (f ((fn [x] x) x))))
+(fn [f] (fn [x] (f x)))
+(def one (fn [f] (fn [x] (f x))))
+
+;; define `two`
+(add-1 one)
+(fn [f] (fn [x] (f (((fn [f] (fn [x] (f x))) f) x))))
+(fn [f] (fn [x] (f ((fn [x] (f x)) x))))
+(fn [f] (fn [x] (f (f x))))
+(def two (fn [f] (fn [x] (f (f x)))))
+
+;; TODO: define `+`
