eae9d96c66a868515a16e4ad18146244233b065f
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2019-02-18 15:39:29 +0900

    Exercise 3.51

--- src/sicp/chapter3/4_1.clj
+++ src/sicp/chapter3/4_1.clj
@@ -3,9 +3,3 @@
 ;;;; 3.4  Concurrency: Time Is of the Essence
 
 ;;; 3.4.1  The Nature of Time in Concurrent Systems
-
-;; Exercise 2.51
-
-(defn show [x]
-  (println x)
-  x)
--- src/sicp/chapter3/5_1.clj
+++ src/sicp/chapter3/5_1.clj
@@ -3,3 +3,13 @@
 ;;;; 3.5  Streams
 
 ;;; 3.5.1  Streams Are Delayed Lists
+
+;; Exercise 3.51
+(defn show [x]
+  (println x)
+  x)
+
+(def x (map show (range 0 (inc 10))))
+
+#_(nth x 5)
+#_(nth x 7)
