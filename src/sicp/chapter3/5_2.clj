12cc6270cd62ed7151fb4285fdc4167baf3cd4b6
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2019-02-21 19:18:26 +0900

    Exercise 3.53, 3.54

--- src/sicp/chapter3/5_2.clj
+++ src/sicp/chapter3/5_2.clj
@@ -3,3 +3,15 @@
 ;;;; 3.5  Streams
 
 ;;; 3.5.2  Infinite Streams
+
+;; Exercise 3.53
+(def s (lazy-seq (cons 1 (map + s s))))
+
+;; Exercise 3.54
+(defn mul-streams [s1 s2]
+  (map * s1 s2))
+
+(def integers (rest (range)))
+
+(def factorials
+  (lazy-seq (cons 1 (mul-streams (rest integers) factorials))))
