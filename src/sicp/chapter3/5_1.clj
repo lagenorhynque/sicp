a49ca87fcae226bdfbcfff4f22b8a1e470098baf
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2019-02-22 20:20:25 +0900

    Exercise 3.50

--- src/sicp/chapter3/5_1.clj
+++ src/sicp/chapter3/5_1.clj
@@ -4,12 +4,21 @@
 
 ;;; 3.5.1  Streams Are Delayed Lists
 
+;; Exercise 3.50
+(defn stream-map [proc & argstreams]
+  (lazy-seq
+   (if (empty? (first argstreams))
+     nil
+     (cons
+      (apply proc (map first argstreams))
+      (apply stream-map
+             (cons proc (map rest argstreams)))))))
+
 ;; Exercise 3.51
 (defn show [x]
   (println x)
   x)
 
 (def x (map show (range 0 (inc 10))))
-
 #_(nth x 5)
 #_(nth x 7)
