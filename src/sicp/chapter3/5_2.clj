4408bf4973afb35687ebd727921ac80467a85414
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2019-02-24 23:54:53 +0900

    Improve Exercise 3.53 implementation

--- src/sicp/chapter3/5_2.clj
+++ src/sicp/chapter3/5_2.clj
@@ -5,7 +5,12 @@
 ;;; 3.5.2  Infinite Streams
 
 ;; Exercise 3.53
-(def s (lazy-seq (cons 1 (map + s s))))
+(defn add-streams [s1 s2]
+  (map + s1 s2))
+
+(def s (lazy-seq (cons 1 (add-streams s s))))
+
+#_(take 10 s)
 
 ;; Exercise 3.54
 (defn mul-streams [s1 s2]
@@ -15,3 +20,5 @@
 
 (def factorials
   (lazy-seq (cons 1 (mul-streams (rest integers) factorials))))
+
+#_(take 10 factorials)
