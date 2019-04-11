51975a49f0c950323870a05e19a22b9fb5b8a9b9
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2019-04-11 20:07:44 +0900

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/math.combinatorics "0.1.4"]
+                 [org.clojure/math.combinatorics "0.1.5"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [dorothy "0.0.7"]]
   :profiles {:dev {:source-paths ["dev/src"]
                    :dependencies [[orchestra "2019.02.06-1"]
                                   [org.clojure/tools.namespace "0.2.11"]]
-                   :plugins [[lein-eftest "0.5.4"]]}})
+                   :plugins [[lein-eftest "0.5.7"]]}})
